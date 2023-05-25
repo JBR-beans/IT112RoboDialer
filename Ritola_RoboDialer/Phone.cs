3e15fe450469f7bf4a262f4818680785b960fe8a
RustyBeans <107262647+RustyBeans@users.noreply.github.com>
2023-05-25 13:19:50 -0700

    first commit

---
+++ Ritola_RoboDialer/Phone.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Ritola_RoboDialer
+{
+	class Phone
+	{
+		protected string phoneNumber;
+		protected string companyName;
+		protected string phoneType;
+
+		public Phone(string phoneNumber, string companyName, string phoneType)
+		{
+			this.phoneNumber = phoneNumber;
+			this.companyName = companyName;
+			this.phoneType = phoneType;
+		}
+
+		public virtual string Dial()
+		{
+			if (phoneType == "1")
+			{
+				HomePhone home = new HomePhone(phoneNumber, companyName, phoneType);
+				return home.Dial();
+			}
+			if (phoneType == "2")
+			{
+				CellPhone cell = new CellPhone(phoneNumber, companyName, phoneType); 
+				return cell.Dial();
+			}
+
+			return "Phone type must be indicated";
+		}
+	}
+
+	class HomePhone : Phone
+	{
+		public HomePhone(string phoneNumber, string companyName, string phoneType)
+			: base(phoneNumber, companyName, phoneType)
+		{
+		}
+
+		public override string Dial()
+		{
+			string homeNumber;
+			homeNumber = phoneNumber;
+
+			return "Dialing " + companyName + " using " + homeNumber + "...";
+		}
+	}
+
+	class CellPhone : Phone
+	{
+		public CellPhone(string phoneNumber, string companyName, string phoneType)
+			: base(phoneNumber, companyName, phoneType)
+		{
+		}
+
+		public override string Dial()
+		{
+			string cellNumber = phoneNumber;
+
+			cellNumber = AppendCountryCode("USA",cellNumber);
+			
+			return "Dialing " + companyName + " using " + cellNumber + "...";
+		}
+		public string AppendCountryCode(string region, string number)
+		{
+			if (region == "USA")
+			{
+				number = "1+" + number;
+			}
+			return number;
+		}
+	}
+}
---
+++ Ritola_RoboDialer/Program.cs
@@ -0,0 +1,33 @@
+﻿// Juliea Ritola
+// IT112
+// NOTES: method AppendCountryCode in class CellPhone shows how you can expand upon the country code dialing functionality (and program scalabilty) by allowing you to choose a region, hard-coded to just "USA" for the sake of the project.
+// BEHAVIORS NOT IMPLIMENTED AND WHY: Didn't have to cut anything out.
+
+using System;
+
+namespace Ritola_RoboDialer
+{
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Phone[] dialingList = new Phone[10];
+			dialingList[0] = new Phone("(303) 985-5060", "CompuTest", "1");
+			dialingList[1] = new Phone("(603) 532-4123", "Curtis Manufacturing", "2");
+			dialingList[2] = new Phone("(800) 876-2524", "Data Functions", "1");
+			dialingList[3] = new Phone("(708) 397-3330", "Donnay Repair", "1");
+			dialingList[4] = new Phone("(360) 434-3894", "ErgoNomic Inc", "1");
+			dialingList[5] = new Phone("(800) 969-4374", "ErgoSource", "1");
+			dialingList[6] = new Phone("(800) 874-8527", "Fox Bay Industries", "2");
+			dialingList[7] = new Phone("(800) 545-6254", "Glare-Guard", "2");
+			dialingList[8] = new Phone("(407) 783-6641", "Hazard Comm Specialists", "2");
+			dialingList[9] = new Phone("(714) 472-4409", "Komfort Support", "2");
+
+			foreach (Phone phone in dialingList)
+			{
+				Console.WriteLine(phone.Dial());
+			}
+		}
+	}
+}
